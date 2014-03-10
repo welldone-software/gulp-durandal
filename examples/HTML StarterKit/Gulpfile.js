14a65535b8c4e31b4c2ce3e0606d2392c1790758
vzaidman <vzaidman@gmail.com>
2014-03-10 14:09:42 +0200

    added an example of starterkit build with and without almond.

---
+++ examples/HTML StarterKit/Gulpfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    rename = require('gulp-rename'),
+    htmlReplace = require('gulp-html-replace'),
+    durandal = require(/*'gulp-durandal'*/ '../../index');
+
+var dest = '../Starterkit Dist';
+
+var extraModules = [
+    'plugins/widget',
+    'plugins/dialog',
+    'plugins/router',
+    'transitions/entrance'
+];
+
+﻿gulp.task('default', ['build', 'watch']);
+
+﻿gulp.task('build', ['statics', 'durandal', 'durandal-almond', 'index-almond']);
+
+﻿gulp.task('watch', function () {
+    gulp.watch(['app/**/*', 'css/**/*'], ['build']); //rebuild durandal when sources change
+});
+
+gulp.task('statics', function(){
+    return gulp.src(['**/*.png', '**/*.css', '**/*.ttf', '**/*.woff',
+            'index.html', '**/require.js'])
+        .pipe(gulp.dest(dest));
+});
+
+﻿gulp.task('durandal', function(){
+    return durandal({
+            // needed since not explicitly required by any 'define([...])',
+            // but rather loaded dynamically by convention
+            extraModules: extraModules
+        })
+        .pipe(gulp.dest(dest + '/app'))
+        .pipe(livereload());
+});
+
+﻿gulp.task('durandal-almond', function(){
+    return durandal({
+            // needed since not explicitly required by any 'define([...])',
+            // but rather loaded dynamically by convention
+            extraModules: extraModules,
+            almond: true
+        })
+        .pipe(gulp.dest(dest))
+        .pipe(livereload());
+});
+
+gulp.task('index-almond', function(){
+    return gulp.src('index.html')
+        .pipe(htmlReplace('js', 'main.js'))
+        .pipe(rename('index-almond.html'))
+        .pipe(gulp.dest(dest));
+});
